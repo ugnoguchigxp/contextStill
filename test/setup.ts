ef1b86d9a27f13950c22532a37c56b685478a300
ugnoguchigxp <ugnoguchigxp@gmail.com>
2026-06-11 11:51:33 +0900

    test: Testing Library の asyncUtilTimeout を引き上げ、検証環境でのタイムアウトエラーを解消

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,8 @@
-import { cleanup } from "@testing-library/react";
+import { cleanup, configure } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { afterEach } from "vitest";
+
+configure({ asyncUtilTimeout: 15000 });
 
 afterEach(() => {
   cleanup();
